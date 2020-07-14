36260fa7d73138c9e5fdbd03eed41e5ab662a647
jsbourdon <bourdon.js@gmail.com>
2020-07-14 08:33:12 -0400

    SQL sucks, so why bother

--- test.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM 
-  (
-     SELECT Mesh.ID as Mesh_ID, Mesh.Name, SubMesh.ID as SubMesh_ID, SubMesh.IndexBufferID, SubMesh.MaterialID 
-	 FROM Mesh 
-	 INNER JOIN SubMesh 
-	 ON Mesh.ID = SubMesh.MeshID
-   ) AS SubMeshAggregate
-INNER JOIN SubMeshVertexStreams ON SubMeshAggregate.SubMesh_ID = SubMeshVertexStreams.SubMeshID;
